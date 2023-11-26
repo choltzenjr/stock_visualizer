6e28e9a705b84b511936ba2e586419fb5c50fc5c
Cade Holtzen <choltzenjr@gmail.com>
2023-11-25 23:47:02 -0600

    put in correct test file

--- mod13_cdhq97.py
+++ mod13_cdhq97.py
@@ -1,64 +1,43 @@
-from main import *
-import datetime
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from stock import main
 
+class TestStockVisualizer(unittest.TestCase):
 
+    def validate_input(self, symbol, chart_type, time_series, start_date, end_date):
+        # Implement your validation logic here
+        return True
 
+    @patch('builtins.input', side_effect=['AAPL', 'line', '2', '2022-01-01', '2022-01-31'])
+    def test_main_valid_input(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main(validator=self.validate_input)
+            output = mock_stdout.getvalue().strip()
+            self.assertIn('AAPL Prices from 2022-01-01 to 2022-01-31', output)
+            self.assertNotIn('Invalid', output)
 
-# symbol: capitalized, 1-7 alpha characters
-def symbol_test():
-    #1 can use this to test 
-    # name = "GOOGL"
-    
-    #2 
-    name = get_symbol()
-            
-    result = "symbol test = ok"
-    #a.) another check
-    if name != name.upper():
-        result = "need to be capitalized"
-    if len(name) < 1 or len(name) > 7:
-        result = "need to be 1-7 alphabets"
-    if not name.isalpha():
-        result = "needs to be all alphabets"
-    return result
-    
+    @patch('builtins.input', side_effect=['AAPL', 'invalid_chart_type', '2', '2022-01-01', '2022-01-31'])
+    def test_main_invalid_chart_type(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main(validator=self.validate_input)
+            output = mock_stdout.getvalue().strip()
+            self.assertIn('Invalid chart type. Please try again.', output)
 
+    @patch('builtins.input', side_effect=['AAPL', 'line', 'invalid_time_series', '2022-01-01', '2022-01-31'])
+    def test_main_invalid_time_series(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main(validator=self.validate_input)
+            output = mock_stdout.getvalue().strip()
+            self.assertIn('Enter the number:', output)
+            self.assertIn('Invalid chart type. Please try again.', output)
 
+    @patch('builtins.input', side_effect=['AAPL', 'line', '2', '2022-01-31', '2022-01-01'])
+    def test_main_invalid_date_range(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main(validator=self.validate_input)
+            output = mock_stdout.getvalue().strip()
+            self.assertIn('Invalid date range. Please try again.', output)
 
-# time series: 1 numeric character, 1 - 4
-def time_series_test():
-     #choose option below
-    # 1 test
-    #user = '1'
-
-    # 2 test
-    user = get_time_series_function()
-    
-    choice = ['1','2','3','4']
-
-
-    if user in choice:
-        result = "time_series test = ok"
-    else:
-        result = "time_series test = fail"
-    return result
-
-    
-
-# start date: date type YYYY-MM-DD
-# end date: date type YYYY-MM-DD
-def start_end_dates_test():
-    start_date, end_date = get_dates()
-    result = "date test = ok"
-    try:
-        datetime.date.fromisoformat(start_date)
-        datetime.date.fromisoformat(end_date)
-    except ValueError:
-        result = "date test = fail, enter date in YYYY-MM-DD format"
-
-    return result
-    
-
-
-
-print(start_end_dates_test())
+if __name__ == '__main__':
+    unittest.main()
